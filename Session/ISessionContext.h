42c73e55f4b56593a45abfbf3061f72aa45b967e
KommyButterCream <rylynn.snap@gmail.com>
2026-05-07 17:54:16 +0900

    Add ISessionContext.h file

---
+++ Session/ISessionContext.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+
+#ifdef BUILD_IOCP_ENGINE_DLL
+#define IOCP_ENGINE_API __declspec(dllexport)
+#else
+#define IOCP_ENGINE_API __declspec(dllimport)
+#endif
+
+class IOCP_ENGINE_API SessionContext
+{
+public:
+	virtual ~SessionContext() = default;
+};
